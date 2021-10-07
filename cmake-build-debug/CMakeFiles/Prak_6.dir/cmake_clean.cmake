78911e4ca7e9402567d49c493b115b1931c0e43d
dafex301 <lfahrel@gmail.com>
2021-10-07 20:30:49 +0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Prak_6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prak_6.dir/lstack.c.obj"
+  "CMakeFiles/Prak_6.dir/mainlstack.c.obj"
+  "Prak_6.exe"
+  "Prak_6.exe.manifest"
+  "Prak_6.pdb"
+  "libPrak_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Prak_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
